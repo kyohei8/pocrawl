bc4c62c2d457f0f5182b87a7f52fcd87a9cea76e
kyohei8 <tsukuda.kyouhei@gmail.com>
2013-05-05 12:44:47 +0900

    change database URL

--- models.rb
+++ models.rb
@@ -1,7 +1,9 @@
 require 'rubygems'
 require 'dm-core'
 
-DataMapper.setup(:default, "sqlite3:///Users/kyohei/Dropbox/private/workspace/pocroll/pocroll.db" || ENV['DATABASE_URL'] || 'postgres://x:x@ec2-54-235-155-40.compute-1.amazonaws.com:5432/de70p0tjphkljg')
+#DataMapper.setup(:default, "sqlite3:///Users/kyohei/Dropbox/private/workspace/pocroll/pocroll.db" || ENV['DATABASE_URL'] || 'postgres://x:x@ec2-54-235-155-40.compute-1.amazonaws.com:5432/de70p0tjphkljg')
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite3:///Users/kyohei/Dropbox/private/workspace/pocroll/pocroll.db')
+
 class User
   include DataMapper::Resource
   property :id, Serial
