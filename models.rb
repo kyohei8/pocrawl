eaa0070d655106dcc3b5f2a37f34ad82bf5ada9c
kyohei8 <tsukuda.kyouhei@gmail.com>
2013-07-02 03:20:28 +0900

    mod travis

--- models.rb
+++ models.rb
@@ -1,7 +1,7 @@
 require 'rubygems'
 require 'dm-core'
 
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "sqlite3://#{Dir.pwd}/db/pocrawl.db")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || 'sqlite::memory:' ||  "sqlite3://#{Dir.pwd}/db/pocrawl.db")
 
 class User
   include DataMapper::Resource
